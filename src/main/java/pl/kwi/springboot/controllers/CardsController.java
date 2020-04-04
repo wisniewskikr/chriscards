4eaccbe4c5ef39f50553a3ab6d42a9c709b1e539
Wisniewski <wisniewk@emea.roche.com>
2020-04-04 10:29:04 +0200

    Init new card

--- src/main/java/pl/kwi/springboot/controllers/CardsController.java
+++ src/main/java/pl/kwi/springboot/controllers/CardsController.java
@@ -7,13 +7,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import pl.kwi.springboot.commands.CardsCommand;
 
 @Controller
-@RequestMapping(value="/cards")
 public class CardsController {
 
-	@RequestMapping
-	public String displayPage(
+	@RequestMapping(value="/cards")
+	public String cards(
 			@ModelAttribute("command") CardsCommand command) {
 		return "cards";
+	}
+	
+	@RequestMapping(value="/newCard")
+	public String newCard(
+			@ModelAttribute("command") CardsCommand command) {
+		return "newCard";
 	}
 	
 }
