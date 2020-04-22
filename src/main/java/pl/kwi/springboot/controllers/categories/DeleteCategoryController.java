0e6a7c304a1cf960ad277c9cb07c3c3c69864132
Wisniewski <wisniewk@emea.roche.com>
2020-04-22 07:20:19 +0200

    Implement delete category

---
+++ src/main/java/pl/kwi/springboot/controllers/categories/DeleteCategoryController.java
@@ -0,0 +1,49 @@
+package pl.kwi.springboot.controllers.categories;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import pl.kwi.springboot.commands.categories.DeleteCategoryCommand;
+import pl.kwi.springboot.db.entities.CategoryEntity;
+import pl.kwi.springboot.db.repositories.CategoryRepository;
+
+@Controller
+public class DeleteCategoryController {
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+
+	@RequestMapping(value="/deleteCategory")
+	public String displayDeleteCategory(
+			@ModelAttribute("command") DeleteCategoryCommand command) {
+		
+		command.setCategories(categoryRepository.findAll());
+		
+		return "categories/deleteCategory";
+		
+	}
+	
+	@RequestMapping(value="/deleteCategory", method = RequestMethod.POST)
+	public String deleteCategory(
+			@ModelAttribute("command") DeleteCategoryCommand command,
+			BindingResult bindingResult) {
+		
+		if (StringUtils.isBlank(command.getSelectedCategory())) {
+			command.setCategories(categoryRepository.findAll());
+			bindingResult.rejectValue("selectedCategory", null, "Proszę wybrać kategorię do usunięcia");			
+			return "categories/deleteCategory";
+		}
+		
+		CategoryEntity category = categoryRepository.findById(Long.valueOf(command.getSelectedCategory())).get();
+		categoryRepository.delete(category);
+		
+		return "redirect:categories";
+		
+	}
+	
+}
