a0c2fa9399b90b93b22298ac2e7f2b6990dadf44
Wisniewski <wisniewk@emea.roche.com>
2020-04-25 09:34:13 +0200

    Add words

--- src/main/java/pl/kwi/springboot/commands/cards/NewCardCommand.java
+++ src/main/java/pl/kwi/springboot/commands/cards/NewCardCommand.java
@@ -7,6 +7,16 @@ public class NewCardCommand {
 	
 	private Iterable<CategoryEntity> categories;
 	private String selectedCategory;
+	private String polishWord;
+	private String polishSentence;
+	private String englishWord;
+	private String englishSentence;
+	private String russianWord;
+	private String russianSentence;
+	private String spainWord;
+	private String spainSentence;
+	private String germanWord;
+	private String germanSentence;
 
 	
 	public Iterable<CategoryEntity> getCategories() {
@@ -21,6 +31,75 @@ public class NewCardCommand {
 	}
 	public void setSelectedCategory(String selectedCategory) {
 		this.selectedCategory = selectedCategory;
+	}
+	
+	public String getPolishWord() {
+		return polishWord;
+	}
+	public void setPolishWord(String polishWord) {
+		this.polishWord = polishWord;
+	}
+	
+	public String getPolishSentence() {
+		return polishSentence;
+	}
+	public void setPolishSentence(String polishSentence) {
+		this.polishSentence = polishSentence;
+	}
+	
+	public String getEnglishWord() {
+		return englishWord;
+	}
+	public void setEnglishWord(String englishWord) {
+		this.englishWord = englishWord;
+	}
+	
+	public String getEnglishSentence() {
+		return englishSentence;
+	}
+	public void setEnglishSentence(String englishSentence) {
+		this.englishSentence = englishSentence;
+	}
+	
+	public String getRussianWord() {
+		return russianWord;
+	}
+	public void setRussianWord(String russianWord) {
+		this.russianWord = russianWord;
+	}
+	public String getRussianSentence() {
+		return russianSentence;
+	}
+	
+	public void setRussianSentence(String russianSentence) {
+		this.russianSentence = russianSentence;
+	}
+	public String getSpainWord() {
+		return spainWord;
+	}
+	
+	public void setSpainWord(String spainWord) {
+		this.spainWord = spainWord;
+	}
+	public String getSpainSentence() {
+		return spainSentence;
+	}
+	
+	public void setSpainSentence(String spainSentence) {
+		this.spainSentence = spainSentence;
+	}
+	public String getGermanWord() {
+		return germanWord;
+	}
+	
+	public void setGermanWord(String germanWord) {
+		this.germanWord = germanWord;
+	}
+	public String getGermanSentence() {
+		return germanSentence;
+	}
+	public void setGermanSentence(String germanSentence) {
+		this.germanSentence = germanSentence;
 	}	
 	
 	
