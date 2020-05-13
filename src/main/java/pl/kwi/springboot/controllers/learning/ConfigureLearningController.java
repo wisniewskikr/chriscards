78aaf9bfbf1f6203da1805ea81314cdc6274094e
Wisniewski <wisniewk@emea.roche.com>
2020-05-13 18:07:10 +0200

    Add deck count

--- src/main/java/pl/kwi/springboot/controllers/learning/ConfigureLearningController.java
+++ src/main/java/pl/kwi/springboot/controllers/learning/ConfigureLearningController.java
@@ -4,13 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import pl.kwi.springboot.commands.learning.ConfigureLearningCommand;
+
 @Controller
 @RequestMapping(value="/learning/configure")
 public class ConfigureLearningController {
 
 	@RequestMapping
 	public String displayPage(
-			@ModelAttribute("command") ConfigureLearningController command) {
+			@ModelAttribute("command") ConfigureLearningCommand command) {
 		return "learning/configureLearning";
 	}
 	
