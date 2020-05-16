9a8a27cbb99e68c13dfdf9692b08f7bb397b0a61
Wisniewski <wisniewk@emea.roche.com>
2020-05-16 09:59:53 +0200

    Handle run action

--- src/main/java/pl/kwi/springboot/controllers/learning/ConfigureLearningController.java
+++ src/main/java/pl/kwi/springboot/controllers/learning/ConfigureLearningController.java
@@ -6,20 +6,31 @@ import java.util.Arrays;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import pl.kwi.springboot.commands.learning.ConfigureLearningCommand;
 import pl.kwi.springboot.enums.LearningModeEnum;
 
 @Controller
-@RequestMapping(value="/learning/configure")
+@RequestMapping(value="/learning")
 public class ConfigureLearningController {
 
-	@RequestMapping
+	@RequestMapping(value="/configure")
 	public String displayPage(
 			@ModelAttribute("command") ConfigureLearningCommand command) {
 		
 		command.setLearningModes(new ArrayList<LearningModeEnum>(Arrays.asList(LearningModeEnum.values())));
 		command.setSelectedLearningMode(LearningModeEnum.MANUAL);
+		
+		return "learning/configureLearning";
+	}
+	
+	@RequestMapping(value="/run", method = RequestMethod.POST)
+	public String run(
+			@ModelAttribute("command") ConfigureLearningCommand command) {
+		
+		System.out.println(command.getDeckCount());
+		System.out.println(command.getSelectedLearningMode().getDisplayText());
 		
 		return "learning/configureLearning";
 	}
