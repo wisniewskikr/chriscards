24b365cd43511f2b705e43d616890e4723d05d7f
Wisniewski <wisniewk@emea.roche.com>
2020-05-21 07:43:07 +0200

    Improvement of language enum

--- src/main/java/pl/kwi/springboot/enums/LanguageEnum.java
+++ src/main/java/pl/kwi/springboot/enums/LanguageEnum.java
@@ -2,6 +2,24 @@ package pl.kwi.springboot.enums;
 
 public enum LanguageEnum {
 	
-	POLISH, ENGLISH, RUSSIAN, SPAIN, GERMAN
+	
+	POLISH("Język Polski"), ENGLISH("Język Angielski"), RUSSIAN("Język Rosyjski"), SPAIN("Język Hiszpański");
+	
+	
+	private String displayText;
 
+	
+	private LanguageEnum(String displayText) {
+		this.displayText = displayText;
+	}
+	
+	
+	public String getDisplayText() {
+		return displayText;
+	}
+	public void setDisplayText(String displayText) {
+		this.displayText = displayText;
+	}
+	
+	
 }
