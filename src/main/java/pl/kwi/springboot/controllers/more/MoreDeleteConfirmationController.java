2d71c8d5ff912c9834a199b18733a2a9ac6c2b81
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2020-08-21 06:28:33 +0200

    Improvement more delete confirmation

--- src/main/java/pl/kwi/springboot/controllers/more/MoreDeleteConfirmationController.java
+++ src/main/java/pl/kwi/springboot/controllers/more/MoreDeleteConfirmationController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import pl.kwi.springboot.commands.more.MoreDeleteConfirmationCommand;
 import pl.kwi.springboot.pagination.checkboxPagination.controllers.AbstrCheckboxPaginationController;
@@ -24,6 +25,19 @@ public class MoreDeleteConfirmationController extends AbstrCheckboxPaginationCon
 		}
 		
 		return "more/moreDeleteConfirmation";
+		
+	}
+	
+	@RequestMapping(value="/run", method = RequestMethod.POST)
+	public String run(
+			@ModelAttribute("command") MoreDeleteConfirmationCommand command) {
+		
+		List<Long> selectedItems = command.getSelectedItems();
+		for (Long long1 : selectedItems) {
+			System.out.println(long1);
+		}
+				
+		return "redirect:/home";
 		
 	}
 
