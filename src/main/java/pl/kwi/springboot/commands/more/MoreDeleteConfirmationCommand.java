a4388cf72809f6cc41027a7478f83dc22cfe9bf8
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2020-08-21 06:38:27 +0200

    Improvement more delete confirmation

--- src/main/java/pl/kwi/springboot/commands/more/MoreDeleteConfirmationCommand.java
+++ src/main/java/pl/kwi/springboot/commands/more/MoreDeleteConfirmationCommand.java
@@ -1,11 +1,13 @@
 package pl.kwi.springboot.commands.more;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MoreDeleteConfirmationCommand {
 	
 	
-	private List<Long> selectedItems;
+	private List<Long> selectedItems = new ArrayList<Long>();
+	private List<String> names = new ArrayList<String>();
 	
 
 	public List<Long> getSelectedItems() {
@@ -13,7 +15,14 @@ public class MoreDeleteConfirmationCommand {
 	}
 	public void setSelectedItems(List<Long> selectedItems) {
 		this.selectedItems = selectedItems;
-	}	
-		
+	}
+	
+	public List<String> getNames() {
+		return names;
+	}
+	public void setNames(List<String> names) {
+		this.names = names;
+	}
+			
 
 }
--- src/main/java/pl/kwi/springboot/controllers/more/MoreDeleteConfirmationController.java
+++ src/main/java/pl/kwi/springboot/controllers/more/MoreDeleteConfirmationController.java
@@ -2,6 +2,7 @@ package pl.kwi.springboot.controllers.more;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +10,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import pl.kwi.springboot.commands.more.MoreDeleteConfirmationCommand;
 import pl.kwi.springboot.pagination.checkboxPagination.controllers.AbstrCheckboxPaginationController;
+import pl.kwi.springboot.services.intf.DeckService;
 
 @Controller
 @RequestMapping(value="/more/delete/confirmation")
 public class MoreDeleteConfirmationController extends AbstrCheckboxPaginationController {
+	
+	
+	@Autowired
+	private DeckService deckService;
 
 
 	@RequestMapping
@@ -20,8 +26,8 @@ public class MoreDeleteConfirmationController extends AbstrCheckboxPaginationCon
 			@ModelAttribute("command") MoreDeleteConfirmationCommand command) {
 		
 		List<Long> selectedItems = command.getSelectedItems();
-		for (Long long1 : selectedItems) {
-			System.out.println(long1);
+		for (Long id : selectedItems) {
+			command.getNames().add(deckService.findById(id).getName());
 		}
 		
 		return "more/moreDeleteConfirmation";
